48296b77678f978ca51ee4c977baad0e0ed15c6a
sherry.zhang <sherry.zhang@ebaotech.com>
2017-03-26 18:06:23 +0800

    update

--- web/backsite/modules/home-manage/homeCtrl.js
+++ web/backsite/modules/home-manage/homeCtrl.js
@@ -1,16 +1,18 @@
 /**
  * 主页内容管理
  */
-MYSITE.value('froalaConfig', {
-		language: 'zh_cn',
-        toolbarInline: false,
-        placeholderText: '请输入编辑内容',
-        pasteAllowedStyleProps: ['font-family', 'font-size', 'color'],
-        toolbarButtons: ['fullscreen', 'print', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'fontFamily', 'fontSize', '|', 'specialCharacters', 'color', 'emoticons', 'inlineStyle', 'paragraphStyle', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', 'insertHR', '-', 'insertLink', 'insertImage', 'insertVideo', 'insertFile', 'insertTable', 'undo', 'redo', 'clearFormatting', 'selectAll', 'html'],
-	    pluginsEnabled: ['link'],
-	    fontFamilySelection: true,
-	    fontSizeSelection: true,
-	    paragraphFormatSelection: true
-    })
-.controller('HomeCtrl', ['$scope', 'HomeService', function ($scope, HomeService) {
-}]);
+MYSITE.controller('HomeCtrl', ['$scope', 'HomeService', 'FileUploader',
+  function ($scope, HomeService, FileUploader) {
+    $scope.myHtml = '';
+
+    var options = {
+      url: 'upload.php/'
+    }
+    var uploader = $scope.uploader = new FileUploader(options);
+    uploader.onBeforeUploadItem = function(item) {
+
+    };
+    uploader.onCompleteItem = function(fileItem, response, status, headers) {
+      console.info('onCompleteItem', fileItem, response, status, headers);
+    };
+  }]);
