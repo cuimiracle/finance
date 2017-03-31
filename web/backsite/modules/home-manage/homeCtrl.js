48f18b7c714a980b9e71f627bc0c7ce7f688b23b
xiaoroad <zxllyf@qq.com>
2017-04-01 00:42:51 +0800

    update

--- web/backsite/modules/home-manage/homeCtrl.js
+++ web/backsite/modules/home-manage/homeCtrl.js
@@ -12,10 +12,15 @@ MYSITE.controller('HomeCtrl', ['$scope', 'HomeService',
 
     $scope.bannerList = [];
     $scope.introList = [];
+    $scope.bigImgList = [];
+    $scope.picTextList = [];
+    $scope.bottomList = [];
 
     $scope.bannerData = _.cloneDeep(initData);
     $scope.introData = _.cloneDeep(initData);
-    delete $scope.introData.link_url;
+    $scope.bigImg = _.cloneDeep(initData);
+    $scope.picText = _.cloneDeep(initData);
+    $scope.bottom = _.cloneDeep(initData);
 
     $scope.myHtml = '';
 
@@ -40,17 +45,14 @@ MYSITE.controller('HomeCtrl', ['$scope', 'HomeService',
     }
     commonGetAll('getAllBanner', 'bannerList');
     commonGetAll('getAllIntro', 'introList');
+    commonGetAll('getBigImgAll', 'bigImgList');
+    commonGetAll('getPicTextAll', 'picTextList');
+    commonGetAll('getBottomContentAll', 'bottomList');
 
     $scope.bannerOk = function () {
       commonAdd($scope.bannerData, 'addBanner', function () {
         commonGetAll('getAllBanner', 'bannerList');
       })
-    };
-
-    $scope.introOk = function () {
-      commonAdd($scope.introData, 'addIntro', function () {
-        commonGetAll('getAllIntro', 'introList');
-      });
     };
 
     $scope.deleteBanner = function (data) {
@@ -59,9 +61,51 @@ MYSITE.controller('HomeCtrl', ['$scope', 'HomeService',
       });
     };
 
+    $scope.introOk = function () {
+      commonAdd($scope.introData, 'addIntro', function () {
+        commonGetAll('getAllIntro', 'introList');
+      });
+    };
+
     $scope.deleteIntro = function (data) {
       commonDelete(data.id, 'deleteIntro', function () {
         commonGetAll('getAllIntro', 'introList');
+      });
+    };
+
+    $scope.bigImgOk = function () {
+      commonAdd($scope.bigImg, 'addBigImg', function () {
+        commonGetAll('getBigImgAll', 'bigImgList');
+      });
+    };
+
+    $scope.deleteBigImg = function (data) {
+      commonDelete(data.id, 'deleteBigImg', function () {
+        commonGetAll('getBigImgAll', 'bigImgList');
+      });
+    };
+
+    $scope.picTextOk = function () {
+      commonAdd($scope.picText, 'addPicText', function () {
+        commonGetAll('getPicTextAll', 'picTextList');
+      });
+    };
+
+    $scope.deletePicText = function (data) {
+      commonDelete(data.id, 'deletePicText', function () {
+        commonGetAll('getPicTextAll', 'picTextList');
+      });
+    };
+
+    $scope.bottomOk = function () {
+      commonAdd($scope.bottom, 'addBottomContent', function () {
+        commonGetAll('getBottomContentAll', 'bottomList');
+      });
+    };
+
+    $scope.deleteBottom = function (data) {
+      commonDelete(data.id, 'deleteBottomContent', function () {
+        commonGetAll('getBottomContentAll', 'bottomList');
       });
     };
   }]);
