20a810b85eed0741b648e7d0072a733404bb9b1a
xiaoroad <zxllyf@qq.com>
2017-04-04 16:49:18 +0800

    update

--- web/backsite/modules/common/login.js
+++ web/backsite/modules/common/login.js
@@ -1,4 +1,4 @@
-MYSITE.controller('LoginCtrl', function ($scope, Service) {
+MYSITE.controller('LoginCtrl', function ($scope, Service, $state) {
   $scope.data = {
     username: '',
     password: ''
@@ -11,11 +11,17 @@ MYSITE.controller('LoginCtrl', function ($scope, Service) {
   $scope.login = function () {
     Service.login($scope.data).then(function (res) {
       console.log('login', res);
+      if (res.data.result == 'true') {
+        $state.go('homeManage');
+      }
     });
   };
   $scope.register = function () {
     Service.register($scope.registerData).then(function (res) {
       console.log('register', res);
+      if (res.data.result == 'true') {
+        alert('注册成功，请登录');
+      }
     });
   };
 });
