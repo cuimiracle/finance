110c7f78f7a8ab929b55b9717baa450d6d8f513f
xiaoroad <zxllyf@qq.com>
2017-04-06 17:55:16 +0800

    update

--- web/backsite/modules/home/homeCtrl.js
+++ web/backsite/modules/home/homeCtrl.js
@@ -40,4 +40,21 @@ MYSITE.controller('HomeCtrl', ['$scope', 'Service', 'PageMap', 'InitData',
         });
       }
     });
+
+    var uploader = $scope.uploader = new FileUploader({
+      url: '../index.php?r=upload-file/photos',
+      headers: { 'Content-Transfer-Encoding': 'utf-8' }
+    });
+
+    uploader.onSuccessItem = function(fileItem, response, status, headers) {
+      console.log('success', fileItem, response);
+    };
+    var imageFilter = {
+      name: 'imageFilter',
+      fn: function(item /*{File|FileLikeObject}*/, options) {
+        var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+        return '|jpg|png|jpeg|gif|'.indexOf(type) !== -1;
+      }
+    }
+    $scope.uploader.filters.push(imageFilter);
   }]);
